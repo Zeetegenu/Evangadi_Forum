8df38cb2ec47300278893b51752fd4cd919789ec
Zeetegenu <zebibtegenu@outlook.com>
2024-10-16 23:12:46 -0400

    api int

--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
   // baseURL: "http://localhost:5500/api/",
-  baseURL: "https://evangadiforum-yalv.onrender.com/api/",
+  baseURL: "https://evangadiforum-1-hh8q.onrender.com/api/",
   // baseURL: "https://evangadi-forum-backend-xi.vercel.app/api/",
 });
 export default instance;
